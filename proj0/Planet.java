ae1c59c0a7be5993a6bab48a7b875b4ac3694af6
jeff <jeffyan167@gmail.com>
2024-01-18 21:31:04 +0800

    project 0 was done until update

---
+++ proj0/Planet.java
@@ -0,0 +1,67 @@
+public class Planet {
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+
+    static final double G = 6.67e-11;
+    public Planet(double xP, double yP, double xV, double yV, double m, String img) {
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = img;
+    }
+    public Planet(Planet p) {
+        xxPos = p.xxPos;
+        yyPos = p.yyPos;
+        xxVel = p.xxVel;
+        yyVel = p.yyVel;
+        mass = p.mass;
+        imgFileName = p.imgFileName;
+    }
+
+    public double calcDistance(Planet p) {
+        return Math.pow(Math.pow(this.xxPos - p.xxPos, 2) + Math.pow(this.yyPos - p.yyPos, 2), 0.5);
+    }
+
+    public double calcForceExertedBy(Planet p) {
+        return (G*this.mass*p.mass/(Math.pow(this.calcDistance(p), 2)));
+    }
+
+    public double calcAngle(Planet p) {
+        return Math.atan((this.yyPos - p.yyPos)/(this.xxPos - p.xxPos));
+    }
+    public double calcForceExertedByX(Planet p) {
+        return this.calcForceExertedBy(p)*Math.cos(this.calcAngle(p));
+    }
+
+    public double calcForceExertedByY(Planet p) {
+        return this.calcForceExertedBy(p)*Math.sin(this.calcAngle(p));
+    }
+
+    public double calcNetForceExertedByX(Planet[] p) {
+        double sum = 0;
+        for (Planet planet : p) {
+            if (planet.equals(this)) {
+                continue;
+            }
+            sum += this.calcForceExertedByX(planet);
+        }
+        return sum;
+    }
+
+    public double calcNetForceExertedByY(Planet[] p) {
+        double sum = 0;
+        for (Planet planet : p) {
+            if (planet.equals(this)) {
+                continue;
+            }
+            sum += this.calcForceExertedByY(planet);
+        }
+        return sum;
+    }
+}
