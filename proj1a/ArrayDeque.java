be1034082dd2b681f7f6e66a09d4611e2e6af912
Jeff Yan <jeffyan167@gmail.com>
2024-01-22 13:55:55 +0800

    ArrayDeque和测试已完成，没做低使用修改

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,3 +1,119 @@
-public class ArrayDeque {
-    
+public class ArrayDeque<T> {
+    private T[] array;
+    private int size;
+    private int tail;
+    private int head;
+
+    public ArrayDeque() {
+        array = (T[]) new Object[8];
+        size = 0;
+        tail = 1;
+        head = 0;
+    }
+
+    private void resize(int capacity) {
+        T[] newArray = (T[]) new Object[capacity];
+
+        if (head < tail) {
+            System.arraycopy(array, 0, newArray, 0, size);
+        } else if (head > tail) {
+            int j = 1;
+            for (int i = (head + 1)%(array.length) ; i != tail ; i = (i + 1)%(array.length), j++) {
+                newArray[j] = array[i];
+            }
+            head = 0;
+            tail = j;
+        }
+        array = newArray;
+    }
+
+    private boolean isFull() {
+        if (size == array.length - 2) {
+            return true;
+        }
+        return false;
+    }
+
+    public void addFirst(T item) {
+        if (isFull()) {
+            resize((int)(array.length*1.5));
+        }
+        array[head] = item;
+        head = (head - 1 + array.length) % (array.length);
+        size++;
+    }
+
+    public void addLast(T item) {
+        if (isFull()) {
+            resize((int)(array.length*1.5));
+        }
+        array[tail] = item;
+        tail = (tail + 1)%(array.length);
+        size++;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    } 
+
+    public void printDeque() {
+        if (isEmpty()) {
+            return;
+        }
+        int index = (head + 1)%(array.length);
+        while(index != tail) {
+            System.out.print(array[index] + " ");
+            index = (index + 1)%(array.length);
+        }
+
+    }
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        if(tail == 0) {
+            tail = array.length - 1;
+        } else {
+            tail -= 1;
+        }
+        size--;
+
+        if (isLowUsage()) {
+            resize((int)(array.length * 0.5));
+        }
+
+        return array[tail];
+    }
+
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        head = (head + 1)%(array.length);
+        size--;
+
+        if (isLowUsage()) {
+            resize((int)(array.length * 0.5));
+        }
+        return array[head];
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size || isEmpty()) {
+            return null;
+        }
+        index = (head + 1 + index)%(array.length);
+        return array[index];
+    }
+
+    private boolean isLowUsage() {
+        if (array.length >= 16 && size / array.length < 0.25) {
+            return false;
+        }
+        return false;
+    }
 }
---
+++ proj1a/ArrayDequeTest.java
@@ -0,0 +1,96 @@
+public class ArrayDequeTest {
+    
+    public static boolean checkEmpty(boolean expected, boolean actual) {
+        if (expected != actual) {
+            System.out.println("isEmpty() returned " + actual + ", but expected: " + expected);
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean checkSize(int expected, int actual) {
+        if (expected != actual) {
+            System.out.println("size() returned " + actual + ", but expected: " + expected);
+            return false;
+        }
+        return true;
+    }
+
+    public static void printTestStatus(boolean passed) {
+        if (passed) {
+            System.out.println("Test passed!\n");
+        } else {
+            System.out.println("Test failed!\n");
+        }
+    }
+
+    public static void addIsEmptySizeTest() {
+        System.out.println("Running add/isEmpty/Size test.");
+		System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
+		
+        ArrayDeque<String> ad1 = new ArrayDeque<String>();
+
+        boolean passed = checkEmpty(true, ad1.isEmpty());
+
+        ad1.addFirst("front");
+
+        passed = checkSize(1, ad1.size()) && passed;
+        passed = checkEmpty(false, ad1.isEmpty()) && passed;
+
+        ad1.addLast("middle");
+        passed = checkSize(2, ad1.size()) && passed;
+
+        ad1.addLast("back");
+        passed = checkSize(3, ad1.size()) && passed;
+
+        System.out.println("Printing out deque: ");
+        ad1.printDeque();
+
+        printTestStatus(passed);
+
+    }
+
+    public static void addRemoveTest() {
+        System.out.println("Running add/remove test.");
+
+		System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
+		
+        ArrayDeque<Integer> ad2 = new ArrayDeque<Integer>();
+
+        boolean passed = checkEmpty(true, ad2.isEmpty());
+
+        for (int i = 119 ; i >= 0 ; --i) {
+            ad2.addFirst(i);
+        }
+        passed = checkSize(120, ad2.size()) && passed;
+
+        for (int i = 0 ; i < 30 ; ++i) {
+            ad2.removeLast();
+        }
+        passed = checkSize(90, ad2.size()) && passed;
+
+        for (int i = 90 ; i < 120 ; ++i) {
+            ad2.addLast(i);
+            // System.out.print(ad2.get(i) + " ");
+        }
+        passed = checkSize(120, ad2.size()) && passed;
+
+        for (int i = 0 ; i < 10 ; ++i) {
+            ad2.removeFirst();
+        }
+        passed = checkSize(110, ad2.size()) && passed;
+
+        for (int i = 10 ; i < 120 ; ++i) {
+            // System.out.print(ad2.removeFirst()+" ");
+            passed = (i == ad2.removeFirst()) && passed;
+        }
+
+        printTestStatus(passed);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Running tests.\n");
+        addIsEmptySizeTest();
+        addRemoveTest();
+    }
+}
